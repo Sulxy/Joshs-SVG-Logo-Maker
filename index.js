204f61e4ff891efa155fd366434ff4cd54b69a97
Joshua Hale <joshuahale829@gmail.com>
2024-02-05 15:51:50 -0600

    Wrote index.js file. color is not defined in lib\shapes and index.js

---
+++ index.js
@@ -0,0 +1,117 @@
+const filesystem = require("fs");
+const inquirer = require("inquirer");
+const { Square, Triangle, Circle } = require("./lib/shapes");
+
+class Svg{
+    constructor(){
+        this.textElement = ''
+        this.shapeElement = ''
+        this.color = ''
+    }
+    render(){
+        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`
+    }
+    setTextElement(text, color){
+        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
+    }
+    setShapeElement(shape){
+        this.shapeElement = shape.render()
+
+    }
+}
+
+const questions = [
+    {
+        type: "input",
+        name: "text",
+        message: "TEXT: Enter up to (3) Characters:",
+    },
+    {
+        type: "input",
+        name: "text-color",
+        message: "TEXT COLOR: Enter a color (OR a hexadecimal number):",
+    },
+    {
+        type: "input",
+        name: "shape",
+        message: "SHAPE COLOR: Enter a color (OR a hexadecimal number):",
+    },
+    {
+        type: "list",
+        name: "Background-Shape",
+        message: "Select a Background Shape?",
+        choices: ["Square", "Triangle", "Circle"],
+    },
+];
+
+function writeToFile(fileName, data) {
+	console.log("Writing [" + data + "] to file [" + fileName + "]")
+    filesystem.writeFile(fileName, data, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("logo.svg generated!");
+    });
+}
+
+async function init() {
+    console.log("Starting init");
+	let svgString = "";
+	let svg_file = "logo.svg";
+
+    const answers = await inquirer.prompt(questions);
+
+	let user_text = "";
+	if (answers.text.length > 0 && answers.text.length < 4) {
+		// 1-3 chars for text entry
+		user_text = answers.text;
+	} else {
+		// 0 or 4+ chars, invalid entry
+		console.log("Please use 3 characters or less!");
+        return;
+	}
+	console.log("User text: [" + user_text + "]");
+	//user font color
+	user_font_color = answers["text-color"];
+	console.log("Font color: [" + user_font_color + "] has been selected.");
+	//user shape color
+	user_shape_color = answers.shape;
+	console.log("Shape color: [" + user_shape_color + "] has been selected.");
+	//user shape type
+	user_shape_type = answers["Background-Shape"];
+	console.log("Selected shape: [" + user_shape_type + "]");
+	
+	//user shape
+let user_shape;
+if (user_shape_type === "Square" || user_shape_type === "square") {
+    user_shape = new Square();
+    console.log("User selected Square shape");
+}
+else if (user_shape_type === "Triangle" || user_shape_type === "triangle") {
+    user_shape = new Triangle();
+    console.log("User selected Triangle shape");
+}
+else if (user_shape_type === "Circle" || user_shape_type === "circle") {
+    user_shape = new Circle();
+    console.log("User selected Circle shape");
+}
+else {
+    console.log("Invalid shape!");
+}
+	user_shape.setColor(user_shape_color);
+
+	// Create a new Svg instance and add the shape and text elements to it
+	var svg = new Svg();
+	svg.setTextElement(user_text, user_font_color);
+	svg.setShapeElement(user_shape);
+	svgString = svg.render();
+	
+	//Print shape to log
+	console.log("Displaying shape:\n\n" + svgString);
+	//document.getElementById("svg_image").innerHTML = svgString;
+
+	console.log("Shape generation complete!");
+	console.log("Writing shape to file...");
+	writeToFile(svg_file, svgString); 
+}
+init()
